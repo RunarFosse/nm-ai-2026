6fbef6089ff652b4d48751dc83312f48f8023b4d
Runar Fosse <rfosse01@gmail.com>
2026-02-28 00:14:00 +0100

    Add tqdm progress bar

--- Grocery Bot/main.py
+++ Grocery Bot/main.py
@@ -1,24 +1,29 @@
 import asyncio
 import json
 import websockets
+from tqdm import tqdm
 
 from example_strategy import ExampleStrategy
 
 WS_URL = open(".ws_url").readline()
+ROUNDS = 300
 
 strategy = ExampleStrategy()
  
 async def play():
     async with websockets.connect(WS_URL) as ws:
+        progress = tqdm(total=ROUNDS)
         while True:
             state = json.loads(await ws.recv())
  
             if state["type"] == "game_over":
+                progress.close()
                 print(f"Game over! Score: {state['score']}")
                 break
  
             actions = strategy.predict(state)
             await ws.send(json.dumps({"actions": actions}))
+            progress.update()
 
 if __name__ == "__main__":
     asyncio.run(play())
